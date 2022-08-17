dc203e05bea9646d77dc89ab34424e6739ad8c2c
Gacheruian <ian.gacheru@student.moringaschool.com>
2022-08-17 21:18:49 +0300

    Completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+function saturdayFun(toDo = 'roller-skate') {
+    return `This Saturday, I want to ${toDo}!`;
+  }
+  saturdayFun();
+const mondayWork = function(toDo = "go to the office"){
+    return `This Monday, I will ${toDo}.`;
+}
+mondayWork()
+let wrapAdjective = function(token="*") 
+{
+    return function(adjective="special") 
+    {
+        return `You are ${token}${adjective}${token}!`;
+
+    }
+    
+  }
